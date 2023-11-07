3e7210c69bb5838cba3cdfaed792bdff33eafd7f
Richard Thies <richardjthies95@gmail.com>
2023-11-07 00:25:36 -0600

    Added two more tests

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -14,24 +14,41 @@ class TestHomePage(TestCase):#django will create a test db automatically for eac
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')#compare response to expected template
         self.assertContains(response, 'You have no places in your wishlist')#django test: compare response to content on page
 
-class TestWishlist(TestCase):#seperate test cases for testing a populated db using fixtures dir
-    fixtures = ['test_places']#fixtures is the default django TestCase directory 
+class TestWishlist(TestCase):#seperate test case for testing a populated db using fixtures dir
+    #arrange?
+    fixtures = ['test_places']#fixtures is the default django TestCase directory. Contains populated db
 
     def test_wishlist_contains_not_visited_places(self):
-        response = self.client.get(reverse('place_list'))
-        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
-        self.assertContains(response, 'Tokyo')
-        self.assertContains(response, 'New York')
-        self.assertNotContains(response, 'San Francisco')
-        self.assertNotContains(response, 'Moab')
+        #act?
+        response = self.client.get(reverse('place_list'))#way of combining home_page_url = reverse('place_list') & response = self.client.get(home_page_url) statements
+        #assert?
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')#still the correct template?
+        self.assertContains(response, 'Tokyo')#not visited in test_places
+        self.assertContains(response, 'New York')#not visited in test_places
+        self.assertNotContains(response, 'San Francisco')#visited in test_places
+        self.assertNotContains(response, 'Moab')#visited in test_places
 
+class TestVisited(TestCase):#seperate test case that is very similar to to TestHomePage 
+    def test_visited_page_shows_empty_list_message_for_empty_db(self):#Is this testing the real db? The real db doesn't have empty msg here that would allow this to be true?
+        #arrange?
+        visited_url = reverse('places_visited')#reverse the visited page 
+        #act?
+        response = self.client.get(visited_url)
+        # response = self.client.get(reverse('places_visited'))#alternative combined syntax
+        #assert?
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')#compare response to expected template
+        self.assertContains(response, 'You have not visited any places yet')#django test: compare response to content on page
 
-
-
-
-
-
-
+class TestVisitedPlaces(TestCase):#not quite sure if I understand this one, and how it works compared to TestWishlist
+    fixtures = ['test_places']
+    
+    def test_visited_contains_only_visited_places_(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')#correct template again?
+        self.assertContains(response, 'San Francisco')#how would you change test_places so this can be true? edit fixtures?
+        self.assertContains(response, 'Moab')
+        self.assertNotContains(response, 'Tokyo')
+        self.assertNotContains(response, 'New York')
 
 
 
