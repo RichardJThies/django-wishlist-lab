23897f13701e34606f27d706ac24335338a5f99b
Richard Thies <richardjthies95@gmail.com>
2023-11-08 20:07:20 -0600

    Started part 2, migrations done

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Place(models.Model):#this subclass describes Place table in db with 2 columns
     name = models.CharField(max_length=200)#constraint
     visited = models.BooleanField(default=False)#assuming that users haven't visited places they add
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)#ForeignKey()is built into django models, 'auth.User' is the name of the other table (must be string), null=False is a constraint, cannot be null,
+                                                                                                                                        #on_delete=models.CASCADE deletes all Places associated with a user if they are deleted
+    notes = models.TextField(blank=True, null=True)#TextField has unlimited characters, unlike CharField, notes can be blank/empty, because they aren't required
+    date_visited = models.DateField(blank=True, null=True)#can be blank/empty, because they aren't required
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)#ImageField() needs 1 extra attribute. upload_to='user_images/ points to directory where images are uploaded. can be blank/empty
 
     def __str__(self):
-        return f'{self.name} visited? {self.visited}'#will not be displayed to user, but for the developer in admin console. Doesn't need extensive formatting, just useful information
+        photo_str = self.photo.url if self.photo else 'no photo'#url in this case referes to the filename of the photo. Need if self.photo else 'no photo' to ensure self.photo is not None. Same for self.notes.
+        notes_str = self.notes[100:] if self.notes else 'no notes'#truncate to first 100 characters
+        return f'{self.pk}: {self.name} visited? {self.visited} on {self.date_visited}. Notes: {notes_str}. Photo {photo_str}'#will not be displayed to user, but for the developer in admin console. Doesn't need extensive formatting, 
+                                                                                                                                                                                #just useful information
 
 
 
--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [#list of urls that app will recognize
     path('', views.place_list, name='place_list'),#represents a request. '' is the homepage, use place_list function from views to handle requests to homepage.
     path('about', views.about, name='about'),
     path('visited', views.places_visited, name='places_visited'),
-    path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited')#place_pk is a stand in for a variable name which is an int
+    path('place/<int:place_pk>/was_visited/', views.place_was_visited, name='place_was_visited')#place_pk is a stand in for a variable name which is an int
 ]
 
 
