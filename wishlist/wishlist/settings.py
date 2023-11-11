3f1478922a2a3721f0cf84ff876c0477c1c1a6c7
Richard Thies <richardjthies95@gmail.com>
2023-11-10 22:01:22 -0600

    changed settings.py automatically send a non-logged in user to the admin login page

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Automatically send a non-logged in user to the admin login page
+LOGIN_URL = '/admin/'
